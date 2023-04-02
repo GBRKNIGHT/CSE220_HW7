6fc160b20271ec6a5e125ef728bed6b7739377fd
Yichen Li <96965461+GBRKNIGHT@users.noreply.github.com>
2023-04-02 18:23:55 +0000

    separate string and adjust space done

--- src/hw7.c
+++ src/hw7.c
@@ -23,7 +23,64 @@ matrix_sf* transpose_mat_sf(const matrix_sf *mat) {
     return NULL;
 }
 
-matrix_sf* create_matrix_sf(char name, const char *expr) {
+// create a new string to make sure the number of spaces between each 
+// operator/ bracket/ number equal to 1. 
+// Then separate into a matrix, and return in this standard form. 
+matrix_sf* create_matrix_sf(char name, const char *expr) 
+{
+    char* new_string = (char*)malloc(MAX_LINE_LEN * sizeof(char)); // max = 128
+    int previous_char = 0; // 
+    char* new_str_ptr = new_string;
+    while(*expr != '\n'){
+        if (isspace(*expr))// if this char is a space
+        {
+            if(previous_char != 1)
+            {   // if there is no space and this is not first char, add this space to 
+                // the new string. 
+                *new_str_ptr = *expr;
+                new_str_ptr++;
+            }
+            previous_char = 1;
+            expr++;
+            continue;
+        }
+        if(ispunct(*expr) || *expr == '=') // if this char is a punct or = 
+        {
+            if(previous_char != 1){
+                *new_str_ptr = ' ';
+                new_str_ptr++;
+            }
+            *new_str_ptr = *expr;
+            new_str_ptr++;
+            previous_char = 2;
+            expr++;
+            continue;
+        }
+        if(isdigit(*expr)) // if this char is a number
+        {
+            if(((previous_char == 2) || (previous_char == 4)) && (*(expr - 1) != '-'))
+            {
+                *new_str_ptr = ' ';
+                new_str_ptr++;
+                
+            }
+            *new_str_ptr = *expr;
+            previous_char = 3;
+            new_str_ptr++;
+            expr++;
+            continue;
+        }
+        if(isalnum(*expr))// if this char is a alphaberical char
+        {
+            // assume this alphaberical char will appear at position 0 only. 
+            *new_str_ptr = *expr; 
+            new_str_ptr++;
+            previous_char = 4;
+            expr++;
+            continue;
+        }
+    }
+    printf("%s \n", new_string);
     return NULL;
 }
 
