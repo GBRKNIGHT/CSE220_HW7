a030a16440a8164642e100ad05b0ce1e99d10d3e
Yichen Li <96965461+GBRKNIGHT@users.noreply.github.com>
2023-04-02 20:58:05 +0000

    create function complete, 28 pass

--- src/hw7.c
+++ src/hw7.c
@@ -1,5 +1,17 @@
 #include "hw7.h"
 
+// This is a utility function used during testing. Feel free to adapt the code to implement some of
+// the assignment. Feel equally free to ignore it.
+matrix_sf *copy_matrix(unsigned int num_rows, unsigned int num_cols, int values[]) {
+    matrix_sf *m = malloc(sizeof(matrix_sf)+num_rows*num_cols*sizeof(int));
+    m->name = '?';
+    m->num_rows = num_rows;
+    m->num_cols = num_cols;
+    memcpy(m->values, values, num_rows*num_cols*sizeof(int));
+    return m;
+}
+
+
 bst_sf* insert_bst_sf(matrix_sf *mat, bst_sf *root) {
     return NULL;
 }
@@ -31,7 +43,8 @@ matrix_sf* create_matrix_sf(char name, const char *expr)
     char* new_string = (char*)malloc(MAX_LINE_LEN * sizeof(char)); // max = 128
     int previous_char = 0; // 
     char* new_str_ptr = new_string;
-    while(*expr != '\n'){
+    while((*expr != '\n') && (*expr != '\0'))
+    {
         if (isspace(*expr))// if this char is a space
         {
             if(previous_char != 1)
@@ -80,8 +93,54 @@ matrix_sf* create_matrix_sf(char name, const char *expr)
             continue;
         }
     }
-    printf("%s \n", new_string);
-    return NULL;
+    // printf("%s \n", new_string);
+
+    // create the matrix_sf struct. 
+    char* matrix_ptr = new_string;
+    while (isdigit (*matrix_ptr) == 0)
+    {   // find first number
+        matrix_ptr++;
+    }
+    int num_rows = atoi(matrix_ptr);
+    while(isspace(*matrix_ptr) == 0){
+        matrix_ptr++;
+    }
+    while (isdigit (*matrix_ptr) == 0)
+    {   // find second number
+        matrix_ptr++;
+    }
+    int num_columns = atoi(matrix_ptr); // find second number 
+    while(*matrix_ptr != '['){
+        matrix_ptr++;
+    } // now point to left bracket [
+    int* values = (int*) malloc(sizeof(int) * num_rows * num_columns);
+    int* value_ptr = values;
+    while(*matrix_ptr != ']')
+    { // fill the matrix until right bracket being found. 
+        if(isdigit(*matrix_ptr) || (*matrix_ptr == '-'))
+        {   // fill one element once find a space.
+            *value_ptr = atoi(matrix_ptr);
+            value_ptr++;
+            while (isdigit(*matrix_ptr) || (*matrix_ptr == '-'))
+            {
+                matrix_ptr++;
+            }
+            
+        }
+        matrix_ptr++;
+    } 
+    // value_ptr = 0;
+
+    // store values into matrix and return. 
+    matrix_sf* struct_matrix = copy_matrix(num_rows, num_columns, values);
+    // for(int i = 0; i < (num_columns * num_rows); i++){
+
+    // }
+    free(values);
+    struct_matrix->name = name;
+    free(new_string);
+    
+    return struct_matrix;
 }
 
 char* infix2postfix_sf(char *infix) {
@@ -96,16 +155,7 @@ matrix_sf *execute_script_sf(char *filename) {
    return NULL;
 }
 
-// This is a utility function used during testing. Feel free to adapt the code to implement some of
-// the assignment. Feel equally free to ignore it.
-matrix_sf *copy_matrix(unsigned int num_rows, unsigned int num_cols, int values[]) {
-    matrix_sf *m = malloc(sizeof(matrix_sf)+num_rows*num_cols*sizeof(int));
-    m->name = '?';
-    m->num_rows = num_rows;
-    m->num_cols = num_cols;
-    memcpy(m->values, values, num_rows*num_cols*sizeof(int));
-    return m;
-}
+
 
 // Don't touch this function. It's used by the testing framework.
 // It's been left here in case it helps you debug and test your code.
