a37704c7cf371e17b80d52b05ab9b710c6c393ca
Yichen Li <96965461+GBRKNIGHT@users.noreply.github.com>
2023-04-04 00:14:45 +0000

    free complete, 37 pass

--- src/hw7.c
+++ src/hw7.c
@@ -17,10 +17,22 @@ bst_sf* insert_bst_sf(matrix_sf *mat, bst_sf *root) {
 }
 
 matrix_sf* find_bst_sf(char name, bst_sf *root) {
+    if(root->mat->name == name){
+        return NULL;
+    }
+    bst_sf* cursor = root;
+    while((cursor->left_child != NULL) || (cursor->right_child != NULL) )
+    {
+
+    }
     return NULL;
 }
 
 void free_bst_sf(bst_sf *root) {
+    bst_sf* cursor = root;
+    free_bst_sf(root->left_child);
+    free_bst_sf(root->right_child);
+    free(cursor);
 }
 
 matrix_sf* add_mats_sf(const matrix_sf *mat1, const matrix_sf *mat2) {
@@ -188,6 +200,7 @@ matrix_sf* create_matrix_sf(char name, const char *expr)
 }
 
 char* infix2postfix_sf(char *infix) {
+
     return NULL;
 }
 
@@ -196,7 +209,67 @@ matrix_sf* evaluate_expr_sf(char name, char *expr, bst_sf *root) {
 }
 
 matrix_sf *execute_script_sf(char *filename) {
-   return NULL;
+    // FILE* input = fopen(filename, "r+");
+    // int matrix_count = 0;
+    // int expression_count = 0;
+    // int num_of_lines = 0;
+    // while(1){
+    //     size_t bytes_read;
+    //     size_t size = MAX_LINE_LEN;
+    //     char *string = NULL;
+    //     string = (char *) malloc (size * sizeof(char));
+    //     bytes_read = getline (&string, &size, input); 
+    //     num_of_lines++;
+    //     if (bytes_read == EOF){
+    //         free(string);
+    //         break;
+    //     }    
+    //     if(bytes_read == 0){
+    //         free(string);
+    //         break;
+    //     } 
+    //     if(strstr(string, "[") != 0) // if found bracket
+    //     {
+    //         matrix_count++;
+    //     }
+    //     else
+    //     {
+    //         expression_count++;
+    //     }
+    // }
+    // char* matrixs[MAX_LINE_LEN] = malloc(sizeof(matrixs[MAX_LINE_LEN]) * matrix_count);
+    // char* expression[MAX_LINE_LEN] = malloc(sizeof(expression[MAX_LINE_LEN]) * expression_count);
+    // int which_matrix = 0;
+    // int which_expression = 0;
+    // for(int i = 0; i < num_of_lines; i++)
+    // {
+    //     size_t bytes_read;
+    //     size_t size = MAX_LINE_LEN;
+    //     char *string = NULL;
+    //     string = (char *) malloc (size * sizeof(char));
+    //     bytes_read = getline (&string, &size, input); 
+    //     if (bytes_read == EOF){
+    //         free(string);
+    //         break;
+    //     }    
+    //     if(bytes_read == 0){
+    //         free(string);
+    //         break;
+    //     } 
+    //     if(strstr(string, "[") != 0) // if found bracket
+    //     {
+    //         matrixs[which_matrix] = string;
+    //         which_matrix++;
+    //     }
+    //     else
+    //     {
+    //         expression[which_expression] = string;
+    //         which_expression++;
+    //     }
+    // }
+    // free(matrixs);
+    // free(expression);
+    return NULL;
 }
 
 
